c1f810fe57b6add1e1ae17bfd77788a328f2e4f4
Brady Holt <brady.holt@gmail.com>
2022-06-04 23:38:30 -0600

    Fix shebang with relative paths

---
+++ bin/jsh.mjs
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+import * as path from "path";
+global.jsh_shebang = true;
+
+const scriptPath = path.join(process.argv[2]);
+
+if (!process.argv[2].startsWith("/")) {
+  // Relative path so join with current working directory
+  scriptPath = path.join(process.cwd(), process.argv[2]);
+}
+
+global.jsh_scriptName = path.basename(scriptPath);
+
+import "jsh";
+await import(scriptPath);
