b5fbf548646028bbe5d959732ca7ffac25311589
bradymholt <gh-actions-bradymholt@github.com>
2022-06-17 05:54:07 +0000

    Version 0.15.0

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,4 +1,4 @@
-// jsh - v0.14.0
+// jsh - v0.15.0
 // https://github.com/bradymholt/jsh#README
 
 import { spawnSync } from "child_process";
@@ -339,10 +339,7 @@ const _http = (method, url, data = null, headers = {}) => {
     headers["Accept"] = headers["Accept"] || "*/*";
     headers["Connection"] = headers["Connection"] || "close";
     headers["User-Agent"] = headers["User-Agent"] || "jsh";
-    if (data instanceof stream.Readable) {
-        headers["Transfer-Encoding"] = headers["Transfer-Encoding"] || "chunked";
-    }
-    else if (typeof data == "object") {
+    if (!(data instanceof stream.Readable) && typeof data == "object") {
         // Add JSON headers if needed
         headers["Content-Type"] = headers["Content-Type"] || "application/json; charset=utf-8";
         headers["Accept"] = headers["Accept"] || "application/json";
