9f104bd45ac5780c27323510a674d3b3878e3484
Brady Holt <brady.holt@gmail.com>
2022-06-17 08:03:15 -0600

    Do not rely upon dist/ for local execution

--- bin/jsh-local.mjs
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import * as jsh from "../dist/index.mjs";
-
-const scriptPath = `${process.cwd()}/${process.argv[2]}`;
-
-jsh.setEntryScriptPath(scriptPath);
-jsh.setupArguments(process.argv.slice(3));
-
-await import(scriptPath);
